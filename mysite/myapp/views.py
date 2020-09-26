eced943cd968c328137b12fa40f422cb0d1530bb
Derrick Aragon <aragonderrick23@icloud.com>
2020-09-26 10:33:44 -0700

    Assignment 3 Submission

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,4 +3,29 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("CINS465 Hello World")
+    card = []#list[]
+    for i in range(1, 21):#generate 20 cards
+        if i % 2 != 0:
+            card_dict = {
+                "cardTitle": "Saturn Card #" + str(i),
+                "subtitle": "Subtitle of Card #" + str(i),
+                "info": "Info of Saturn Card #" + str(i),
+                "counter": i
+            }
+        else:
+            card_dict = {
+                "cardTitle": "Uranus Card #" + str(i),
+                "subtitle": "Subtitle of Card #" + str(i),
+                "info": "Info of Uranus Card #" + str(i),
+                "counter": i
+            }
+        card.append(card_dict)
+
+    title = "CINS465"
+    content = "Welcome to my work in progress! CINS465 Hello World!"
+    context = {
+        "title":title,
+        "body":content,
+        "card": card,
+    }
+    return render(request,"base.html", context=context)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '%rie=6^xmiad#w_0n-5(st$1e^covdx_9cgg9b2u#&s=4esd5c'
+
+# Security Warning: dont run with debug turned on
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -113,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
